06b5f0fef7c59a1cd1464ac4430cb27c2117cdee
JustineTrupheme <justine.trupheme@epitech.eu>
2021-02-26 21:11:54 +0100

    my_runner

--- src/my_help.c
+++ src/my_help.c
@@ -10,15 +10,15 @@
 void no_arg(void)
 {
     my_putstr("./my_runner: bad arguments: 0 given but 1 is required");
-    my_putstr("\nretry with -h🤭\n");
+    my_putstr("\nretry with -h\n");
 }
 
 void my_help(void)
 {
-    my_putstr("Finite runner created with CSFML.😛\n\n");
+    my_putstr("Finite runner created with CSFML.\n\n");
     my_putstr("USAGE\n");
     my_putstr(" ./my_runner map.txt\n\n\nOPTIONS\n");
-    my_putstr(" -i\t\tlauch the game in infinity mode.\n -h\t\tprint");
+    my_putstr("\n -h\t\tprint");
     my_putstr(" the usage and quit.\n\n");
     my_putstr("USER INTERACTIONS\n SPACE_KEY\t jump\n RIGHT_KEY\tmove to the");
     my_putstr(" right\n LEFT_KEY\tmove to the left\n\nIN THE START MENU\n");
